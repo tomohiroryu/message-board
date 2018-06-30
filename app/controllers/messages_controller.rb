84621aa59e594e6cd65f2fa76977aa694bd35c40
tomo <ryu@server.fake>
2018-06-30 13:29:01 +0000

    CRUD messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,3 +1,23 @@
-def show
-  @messages=Message.find(params[:id])
+class MessagesController < ApplicationController
+  def index
+    @messages=Message.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
